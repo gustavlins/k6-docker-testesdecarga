ebe4759835e25499c562ddd2e67bf0158298478a
Gustavo Lins <46906500+gustavlins@users.noreply.github.com>
2026-04-14 11:46:20 -0300

    Refatoração do código, limpeza de código.

--- rampVUs.js
+++ rampVUs.js
@@ -1,16 +1,20 @@
 import http from 'k6/http';
-import { sleep, check } from 'k6';
+import { check, sleep } from 'k6';
 
 export const options = {
   stages: [
     { duration: '30s', target: 20 },
-    { duration: '1m30s', target: 10 },
-    { duration: '20s', target: 0 },
-  ]
+    { duration: '1m', target: 20 },
+    { duration: '30s', target: 0 },
+  ],
+  thresholds: {
+    http_req_duration: ['p(95)<500'], 
+    http_req_failed: ['rate<0.01'],   
+  },
 };
 
-export default function() {
-  const res = http.get('https://quickpizza.grafana.com');
-  check(res, { "status is 200": (res) => res.status === 200 });
+export default function () {
+  const res = http.get('https://test.k6.io'); // Usando a API de teste do k6
+  check(res, { 'status was 200': (r) => r.status == 200 });
   sleep(1);
 }
